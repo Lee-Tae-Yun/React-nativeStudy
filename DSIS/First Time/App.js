ab01306d754783e59148b5a5ba4d629fe6627699
이태윤 <58839422+Lee-Tae-Yun@users.noreply.github.com>
2023-05-19 02:02:35 +0900

    Create App.js

---
+++ DSIS/First Time/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native'
+
+const App = () => {
+    return(
+        <SafeAreaView style={{flex:1}}>
+            <View style ={styles.containerPowderblue}>
+                <Text>PowderBlue</Text>
+            </View>
+            <View style = {{flex:2,flexDirection:'row'}} >
+                <View style = {{flex:1}} >
+                    <View style ={styles.containerYellow}>
+                        <Text>Yellow</Text>
+                    </View>
+                    <View style ={styles.containerLavender}>
+                        <Text>Lavender</Text>
+                    </View>
+                </View>
+                <View style ={styles.containerPink}>
+                    <Text>Pink</Text>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+const styles = StyleSheet.create({
+containerPowderblue: {
+    flex: 1,
+    backgroundColor: 'powderblue',
+    alignItems: 'center',
+    justifyContent: 'center'
+},
+containerYellow: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center'
+},
+containerLavender: {
+    flex:2,
+    backgroundColor: 'lavender',
+    alignItems: 'center',
+    justifyContent: 'center'},
+containerPink: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center'
+}
+    
+})
+
+export default App
